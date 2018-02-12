23a398a8b38f8cdbd15aff847d10777c54f84017
Antonio Monje <monje001@cougars.csusm.edu>
2018-02-12 04:45:20 -0800

    First Commit

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+	public void Menu()
+	{
+		Application.LoadLevel (0);
+	}
+	public void RollABall()
+	{
+		Application.LoadLevel (1);
+	}
+	public void SpaceZombies()
+	{
+		Application.LoadLevel (2);
+	}
+	public void Exit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+}
